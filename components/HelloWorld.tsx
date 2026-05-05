63938bf3455df785ba18926e3e975fc96c7a8a5a
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-05-05 18:22:41 +0500

    Initial commit from Flare Codegen (API)

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+export default function HelloWorld() {
+  return (
+    <div className="container">
+      <h1 className="title">Hello, World!</h1>
+      <p className="subtitle">Welcome to my Next.js 14 application.</p>
+      <span className="badge">Built with Next.js &amp; TypeScript</span>
+    </div>
+  );
+}
